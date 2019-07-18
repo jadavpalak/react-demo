151eb29952c2469cf7c5eb98f3241f86d394f6c7
Palak Jadav <palak.jadav@sparsh1.com>
2019-07-18 11:38:24 +0530

    Updates

---
+++ precache-manifest.e5c6921a7bd987ed9c575c0b07b2632a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "461874bcb6154938e43c",
+    "url": "/react-demo/static/css/main.54147838.chunk.css"
+  },
+  {
+    "revision": "461874bcb6154938e43c",
+    "url": "/react-demo/static/js/main.7cdb2b50.chunk.js"
+  },
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  },
+  {
+    "revision": "5cd0cc0b11c2a6a7b60d",
+    "url": "/react-demo/static/css/2.d1045cc5.chunk.css"
+  },
+  {
+    "revision": "5cd0cc0b11c2a6a7b60d",
+    "url": "/react-demo/static/js/2.f84e7227.chunk.js"
+  },
+  {
+    "revision": "19d6fd1ff9d1a7985302aa8a3a761d45",
+    "url": "/react-demo/index.html"
+  }
+];
